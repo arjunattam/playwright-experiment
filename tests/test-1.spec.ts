5bcf6bf5143eac5ff890ab11766bac5c08b0f26e
Arjun Attam <arjunattam@gmail.com>
2023-09-08 21:58:55 +0530

    fix

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,16 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.100ms.live/', {waitUntil: "domcontentloaded"});
-  await page.locator('#navbar').getByRole('button', { name: 'Schedule a Demo' }).click();
-  const fr = 'iframe[title="Calendly Scheduling Page"]';
-  await page.locator(fr).scrollIntoViewIfNeeded();
+test('Demo slot is available for next 2 business days', async ({ page }) => {
+  // 
+  await page.goto('https://www.100ms.live/', { waitUntil: 'domcontentloaded' });
+  await page.getByRole('button', { name: 'Schedule a Demo' }).first().click();
 
-  // Calendly loads widget page
-  await expect(page.frameLocator(fr).getByRole('button', { name: 'Cookie Settings', exact: true })).toBeVisible()
+  //
+  const bookingFrame = 'iframe[title="Calendly Scheduling Page"]';
+  // https://github.com/microsoft/playwright/issues/21054
+  await page.locator(bookingFrame).scrollIntoViewIfNeeded();
 
-  await page.frameLocator(fr).getByRole('button', { name: 'Accept all' }).click();
-  await page.frameLocator(fr).getByLabel('Monday, September 11 - Times available').click();
-  await page.frameLocator(fr).getByRole('button', { name: '10:00am' }).click();
-  await page.frameLocator(fr).getByLabel('Next 10:00am').click();
+  // Handle cookie modal on Calendly iframe
+  // Calendly loads "accept cookies" modal that is localized to geographies (US different from India)
+  // fails because text is localised on CI/local
+  // for local run: npx playwright test --ui
+  // for CI run: npx playwright show-report ~/Downloads/playwright-report/.
+  await expect(page.frameLocator(bookingFrame).getByText('We respect your personal privacy')).toBeVisible();
+  await page.frameLocator(bookingFrame).getByRole('button', { name: 'Close' }).click();
+
+  await page.frameLocator(bookingFrame).getByLabel('Monday, September 11 - Times available').click();
+  await page.frameLocator(bookingFrame).getByRole('button', { name: '10:00am' }).click();
+  await page.frameLocator(bookingFrame).getByLabel('Next 10:00am').click();
 });
